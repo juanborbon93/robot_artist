2b8d7c905a6500b4dfce674cc6469d49dbd7245f
juanborbon93 <juan.borbon@protonmail.com>
2024-02-10 10:11:48 -0800

    improved logging

--- project_init.py
+++ project_init.py
@@ -9,36 +9,57 @@ try:
     from __main__ import __file__ as module_name
 except ImportError:
     module_name = "unknown_module"
+import inspect
 
 module_name = Path(module_name).stem
-LOG_DIR = Path(__file__).parent / f"logs/{module_name}/"
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+LOG_DIR = Path(__file__).parent / f"logs/{module_name}/{timestamp}/"
 LOG_DIR.mkdir(exist_ok=True, parents=True)
 
 load_dotenv()
 
+def get_log_level():
+    log_level = os.environ.get("LOG_LEVEL", "INFO")
+    log_level_map = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL
+    }
+    assert log_level in log_level_map, f"Invalid log level: {log_level}"
+    return log_level_map[log_level]
+
 assert "OPENAI_API_KEY" in os.environ, "API key not found in environment variables"
 
 class SharedLogger:
-    _logger = None
+    _file_handler = None
+    _stream_handler = None
 
     @classmethod
     def get_logger(cls):
         """Returns a shared logger instance. If it doesn't exist, creates one."""
-        if cls._logger is None:
-            cls._logger = logging.getLogger(module_name)  # Use module_name as the logger name
-            cls._logger.setLevel(logging.DEBUG)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        calling_module_name = Path(inspect.stack()[1].filename).name
+        logger = logging.getLogger(calling_module_name)
+        logger.setLevel(get_log_level())
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        if cls._file_handler is None:
             # file handler
-            log_file = LOG_DIR / (datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log")
+            log_file = LOG_DIR / "log.txt"
             file_handler = logging.FileHandler(log_file)
             file_handler.setFormatter(formatter)
+            cls._file_handler = file_handler
+        if cls._stream_handler is None:
             # stream handler
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(formatter)
-            # add handlers
-            cls._logger.addHandler(file_handler)
-            cls._logger.addHandler(stream_handler)
-        return cls._logger
+            cls._stream_handler = stream_handler
+        
+        # add handlers
+        logger.addHandler(cls._file_handler)
+        logger.addHandler(cls._stream_handler)
+        return logger
     
 
 if __name__ == "__main__":
